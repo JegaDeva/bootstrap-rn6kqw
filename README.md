af3c707a738bef0922b0255d2566a923942786ac
JegaDeva <pjegajothi@gmail.com>
2021-10-22 22:47:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bootstrap-rn6kqw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bootstrap-rn6kqw)
---
+++ app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active',
+    },
+    {
+      name: 'Test Account',
+      status: 'inactive',
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown',
+    },
+  ];
+  onStatusChanged(updateInfo: { id: number; newStatus: string }) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
+}
